7d717fe38ce82d7116a6eca033e506534513c63f
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:56 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Go SOCKS5 proxy server and client library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, substrate, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      mkGoLibraryCheck = (import "${substrate}/lib/go-library-check.nix").mkGoLibraryCheck;
+    in {
+      checks.default = mkGoLibraryCheck pkgs {
+        pname = "go-socks5";
+        version = "0.0.0-dev";
+        src = self;
+        vendorHash = "sha256-V8vFSLa9Z2miXVHcceq0W2F4bWacXuG4aey93q0XWuQ="; # COMPUTING
+      };
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ go gopls gotools ];
+      };
+    });
+}
