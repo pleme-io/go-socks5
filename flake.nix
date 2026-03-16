dc78a0520376e93433e98467d2377d1347a7c809
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:23 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,20 +10,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoLibraryCheck = (import "${substrate}/lib/go-library-check.nix").mkGoLibraryCheck;
-    in {
-      checks.default = mkGoLibraryCheck pkgs {
-        pname = "go-socks5";
-        version = "0.0.0-dev";
-        src = self;
-        vendorHash = "sha256-V8vFSLa9Z2miXVHcceq0W2F4bWacXuG4aey93q0XWuQ=";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "library";
+    pname = "go-socks5";
+    vendorHash = "sha256-V8vFSLa9Z2miXVHcceq0W2F4bWacXuG4aey93q0XWuQ=";
+    description = "Go SOCKS5 proxy server and client library";
+    homepage = "https://github.com/pleme-io/go-socks5";
+  };
 }
